6bb51853c07b8b359608e4e77153f14e9fe7145d
Ruben Chavarria <txingo@gmail.com>
2012-09-21 12:48:53 +0200

    green: more than five times the same char

--- rle-compression-javascript/src/main/javascript/Compressor.js
+++ rle-compression-javascript/src/main/javascript/Compressor.js
@@ -1,8 +1,18 @@
 var Compressor = function () {
 
+    // constants
+    var MAX_OCCURRENCES = 5;
+
     // private functions
     var buildPartialOutput = function(nCharacters, character) {
-        return "" + nCharacters + character;
+        var partialOutput = "";
+
+        for ( ; nCharacters > 0; nCharacters -= MAX_OCCURRENCES) {
+            var number = (nCharacters > MAX_OCCURRENCES) ? MAX_OCCURRENCES : nCharacters;
+            partialOutput += "" + number + character;
+        }
+
+        return partialOutput;
     }
 
     // class implementation
