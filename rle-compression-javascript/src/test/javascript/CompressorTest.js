5f781ea3c5bf7fe8bb8acd65d1d942146bee4b6e
Ruben Chavarria <txingo@gmail.com>
2012-09-21 12:35:30 +0200

    red: test for input 'a'

--- rle-compression-javascript/src/test/javascript/CompressorTest.js
+++ rle-compression-javascript/src/test/javascript/CompressorTest.js
@@ -5,3 +5,10 @@ describe('Given an empty input', function(){
         expect(result).toBe("");
     });
 });
+
+describe('Given input: a', function(){
+    it('returns 1a', function(){
+        var result = Compressor.rle("a");
+        expect(result).toBe("1a");
+    });
+});
