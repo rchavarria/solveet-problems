052e11a42a8c833604f8f3e00b969981487415d8
Ruben Chavarria <txingo@gmail.com>
2012-09-24 16:33:45 +0200

    green: testInput_5T_Outputs_TTTTT

--- marvellous-mazes/src/main/groovy/Maze.groovy
+++ marvellous-mazes/src/main/groovy/Maze.groovy
@@ -5,6 +5,21 @@ class Maze {
 	
 	def create(filePath) {
 		def lines = new File(filePath).readLines()
-		"T"
+		def oneLine = lines.join("!")
+
+		def nCharacters = extractNCharacters(oneLine)
+		def character = extractCharacter(oneLine)
+
+		def output = "" 
+		nCharacters.times { output += character }
+		output
+	}
+
+	private extractNCharacters(input) {
+		input[0] as Integer
+	}
+
+	private extractCharacter(input) {
+		input[1] as String
 	}
 }
