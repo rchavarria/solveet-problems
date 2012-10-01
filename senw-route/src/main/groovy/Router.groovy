fffd95c772e9aa1238f9de18e24abd33816f31e7
Ruben Chavarria <txingo@gmail.com>
2012-10-01 11:00:02 +0200

    refactor: compacting code to publish the solution at solveet.com

--- senw-route/src/main/groovy/Router.groovy
+++ senw-route/src/main/groovy/Router.groovy
@@ -1,42 +1,37 @@
 /**
- * Solucion completa y test en: https://github.com/rchavarria/solveet-problems/tree/master/senw-route
+ * Complete solution and tests at: https://github.com/rchavarria/solveet-problems/tree/master/senw-route
  */
 class Router {
     
     def follow(map, route){
         if(route == "") [1, 1]
 
+        try { return performMovements(map, route) }
+        catch (Exception e) { return -1 }
+    }
+
+    private performMovements(map, route) {
         int row = 1
         int column = 1
-
-        try {
-            route.each { movement ->
-                switch(movement) {
-                    case "E":
-                        column++
-                        break
-                    case "S":
-                        row++
-                        break
-                    case "N":
-                        row--
-                        break
-                    case "W":
-                        column--
-                        break
-                }
-                
-                checkWalkableRow(map, row, column)
+        
+        route.each { movement ->
+            switch(movement) {
+                case "E": column++; break
+                case "W": column--; break
+                case "S": row++; break
+                case "N": row--; break
             }
-
-        } catch (Exception e) {
-            return -1
+            
+            checkWalkableRow(map, row, column)
         }
 
         [row, column]
     }
 
-    // throws an exception if you can not walk on the cell
+    /**
+     * throws an exception if you can not walk on the cell
+     * or you walk outside the cell matrix
+     */
     private checkWalkableRow(map, row, column) {
         if(map[row - 1][column - 1] != 1)
             throw new Exception()
