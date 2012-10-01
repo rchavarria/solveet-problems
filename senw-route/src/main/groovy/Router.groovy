348163dbb150fe570ef19864f5262cd7bbf85a19
Ruben Chavarria <txingo@gmail.com>
2012-10-01 10:25:01 +0200

    tests without especifying route

--- senw-route/src/main/groovy/Router.groovy
+++ senw-route/src/main/groovy/Router.groovy
@@ -2,8 +2,8 @@
  * Solucion completa y test en: https://github.com/rchavarria/solveet-problems/tree/master/senw-route
  */
 class Router {
-	
-	def follow(map, route){
-		null
-	}
+    
+    def follow(map, route){
+        [1, 1]
+    }
 }
--- senw-route/src/test/groovy/RouterTest.groovy
+++ senw-route/src/test/groovy/RouterTest.groovy
@@ -4,17 +4,34 @@ import org.junit.Test
 import org.junit.Before
 
 class RouterTest extends GroovyTestCase {
-	
-	def router
+    
+    def router
 
-	@Before
-	public void setUp() {
-		router = new Router()
-	}
+    @Before
+    public void setUp() {
+        router = new Router()
+    }
 
-	@Test
-	public void testFirst() {
-		router.follow(null, null)
-	}
+    @Test
+    public void testSingleCellWithoutRoute() {
+        def map = [ [1] ]
+        def route = ""
+
+        def position = router.follow(map, route)
+        
+        def expectedPosition = [1, 1]
+        assertEquals(expectedPosition, position)
+    }
+
+    @Test
+    public void testSingleRowWithoutRoute() {
+        def map = [ [1, 1, 1, 0] ]
+        def route = ""
+
+        def position = router.follow(map, route)
+        
+        def expectedPosition = [1, 1]
+        assertEquals(expectedPosition, position)
+    }
 
 }
