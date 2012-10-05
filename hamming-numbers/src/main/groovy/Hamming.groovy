d7749f1b3ca8baa62132463e4e5e54e2a9ef40de
Ruben Chavarria <txingo@gmail.com>
2012-10-05 12:43:44 +0200

    solution to hamming numbers problem

---
+++ hamming-numbers/src/main/groovy/Hamming.groovy
@@ -0,0 +1,18 @@
+/**
+ * Complete solution and tests at: https://github.com/rchavarria/solveet-problems/tree/master/hamming-numbers
+ */
+class Hamming {
+    
+    def generate(n){
+        def sucession = [1]
+
+        for(def i = 0; i < n; i++) {
+            sucession << sucession[i] * 2
+                      << sucession[i] * 3
+                      << sucession[i] * 5
+            sucession = sucession.unique()
+        }
+
+        sucession.sort()[0..(n - 1)]
+    }
+}
---
+++ hamming-numbers/src/test/groovy/HammingTest.groovy
@@ -0,0 +1,32 @@
+
+import static org.junit.Assert.*
+import org.junit.Test
+import org.junit.Before
+
+class HammingTest extends GroovyTestCase {
+    
+    def hamming
+
+    @Before
+    public void setUp() {
+        hamming = new Hamming()
+    }
+
+    @Test
+    public void test() {
+        assertEquals([1], hamming.generate(1))
+        assertEquals([1, 2], hamming.generate(2))
+        assertEquals([1, 2, 3], hamming.generate(3))
+        assertEquals([1, 2, 3, 4], hamming.generate(4))
+        assertEquals([1, 2, 3, 4, 5], hamming.generate(5))
+        assertEquals([1, 2, 3, 4, 5, 6], hamming.generate(6))
+
+        assertEquals([1, 2, 3, 4, 5, 6, 8], hamming.generate(7))
+        assertEquals([1, 2, 3, 4, 5, 6, 8, 9], hamming.generate(8))
+        assertEquals([1, 2, 3, 4, 5, 6, 8, 9, 10], hamming.generate(9))
+        assertEquals([1, 2, 3, 4, 5, 6, 8, 9, 10, 12], hamming.generate(10))
+
+        assertEquals([1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15], hamming.generate(11))
+        assertEquals([1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16], hamming.generate(12))
+    }
+}
