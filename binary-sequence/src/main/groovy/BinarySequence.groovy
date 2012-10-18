d680937d415cae143344020b6b8037d7336d2b91
Ruben Chavarria <txingo@gmail.com>
2012-10-18 12:26:57 +0200

    solution completed

--- binary-sequence/src/main/groovy/BinarySequence.groovy
+++ binary-sequence/src/main/groovy/BinarySequence.groovy
@@ -3,7 +3,38 @@
  */
 class BinarySequence {
 
-    def periodicLenght(sequence) {
-        1
+    def periodicLength(sequence) {
+        if(sequence.length() == 1) return 1
+
+        def candidates = (1..sequence.length() / 2)
+
+        def firstCandidate = candidates.find { sequenceLength ->
+            def region = sequence[0..sequenceLength - 1]
+            def repetitions = sequence.length() / sequenceLength as Integer
+
+            (1..repetitions - 1).inject(true) { valid, regionIndex ->
+                def sequenceOffset = regionIndex * sequenceLength
+                def regionMatches = sequence.regionMatches(sequenceOffset, region, 0, sequenceLength)
+
+                valid && regionMatches
+            }
+        }
+
+        firstCandidate ? firstCandidate : 0
     }
 }
+
+// Showing how to use the class
+/*
+BinarySequence bs = new BinarySequence()
+def sequence = "1101111011110111101111"
+def sequenceLength = bs.periodicLength(sequence)
+if (sequenceLength > 0) {
+    def region = sequence[0..sequenceLength - 1]
+    println "La secuencia es periódica"
+    println "Periodo: $region"
+    println "Longitud: $sequenceLength"
+} else {
+    println "La secuencia no es periódica"
+}
+*/
--- binary-sequence/src/test/groovy/BinarySequenceTest.groovy
+++ binary-sequence/src/test/groovy/BinarySequenceTest.groovy
@@ -14,8 +14,65 @@ class BinarySequenceTest extends GroovyTestCase {
 
     @Test
     public void testLenghtOne() {
-        def actual = sequence.periodicLenght("1")
-        def expected = 1
-        assertEquals(expected, actual)
+        assertEquals(1, sequence.periodicLength("1"))
+        assertEquals(1, sequence.periodicLength("0"))
+    }
+
+    @Test
+    public void testLenghtTwo() {
+        assertEquals(1, sequence.periodicLength("11"))
+        assertEquals(1, sequence.periodicLength("00"))
+        
+        assertEquals(0, sequence.periodicLength("01"))
+        assertEquals(0, sequence.periodicLength("10"))
+    }
+
+    @Test
+    public void testLenghtThree() {
+        assertEquals(1, sequence.periodicLength("111"))
+        assertEquals(1, sequence.periodicLength("000"))
+        
+        assertEquals(0, sequence.periodicLength("011"))
+        assertEquals(0, sequence.periodicLength("100"))
+
+        assertEquals(0, sequence.periodicLength("101"))
+        assertEquals(0, sequence.periodicLength("010"))
+    }
+
+    @Test
+    public void testLenghtFour() {
+        assertEquals(1, sequence.periodicLength("1111"))
+        assertEquals(1, sequence.periodicLength("0000"))
+        
+        assertEquals(2, sequence.periodicLength("0101"))
+        assertEquals(2, sequence.periodicLength("1010"))
+
+        assertEquals(0, sequence.periodicLength("1011"))
+        assertEquals(0, sequence.periodicLength("0011"))
+        assertEquals(0, sequence.periodicLength("1101"))
+        assertEquals(0, sequence.periodicLength("0001"))
+    }
+
+    @Test
+    public void testRegionsThatMatch() {
+        assertEquals(5, sequence.periodicLength("10110101101011010110"))
+        assertEquals(5, sequence.periodicLength("1011010110101101011010"))
+
+        assertEquals(6, sequence.periodicLength("110111110111110111"))
+        assertEquals(6, sequence.periodicLength("110111110111110111110"))
+    }
+
+    @Test
+    public void testRegionsThatDontMatch() {
+        assertEquals(0, sequence.periodicLength("101101011011011010110"))
+        assertEquals(0, sequence.periodicLength("10110110110101101011010"))
+
+        assertEquals(0, sequence.periodicLength("1101111101110110111"))
+        assertEquals(0, sequence.periodicLength("1010111110111110111110"))
+    }
+
+    @Test
+    public void testExample() {
+        assertEquals(5, sequence.periodicLength("1101111011110111101111"))
     }
 }
