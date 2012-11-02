ef91b5caf8078cf6ff3e85feb11fc6cc6e265d3b
Ruben Chavarria <txingo@gmail.com>
2012-11-02 12:55:10 +0100

    skeleton for a new solution for route withou loops

---
+++ loopless-route-2/src/main/groovy/LoopesRoute.groovy
@@ -0,0 +1,10 @@
+/**
+ * Complete solution and tests at: https://github.com/rchavarria/solveet-problems/tree/master/loopless-route-2
+ */
+class LoopesRoute {
+
+}
+
+// Showing how to use the class
+/*
+*/
---
+++ loopless-route-2/src/test/groovy/LoopesRouteTest.groovy
@@ -0,0 +1,19 @@
+
+import static org.junit.Assert.*
+import org.junit.Test
+import org.junit.Before
+
+class LoopesRouteTest extends GroovyTestCase {
+    
+    def route
+
+    @Before
+    public void setUp() {
+        route = new LoopesRoute()
+    }
+
+    @Test
+    public void test() {
+        
+    }
+}
