433d79483cab20407bfba3ec6b0a76549b13249d
Ruben Chavarria <txingo@gmail.com>
2012-11-05 10:21:44 +0100

    solution complete

--- loopless-route-2/src/main/groovy/LoopesRoute.groovy
+++ loopless-route-2/src/main/groovy/LoopesRoute.groovy
@@ -3,6 +3,41 @@
  */
 class LoopesRoute {
 
+    def makeItLoopless(path) {
+        def firstStep = [point: [0, 0], movement: ""]
+        def steps = [ firstStep ]
+
+        path.each { goesTo ->
+            def step = nextStep(steps[-1], goesTo)
+            def idx = steps.findIndexOf {
+                it["point"] == step["point"]
+            }
+            
+            if (idx >= 0) {
+                def steps_idx = steps[idx]
+                steps = steps[0..idx]
+            } else {
+                steps << step
+            }
+        }
+
+        steps.inject("") { str, step -> str += step["movement"] }
+    }
+
+    private nextStep(step, goesTo) {
+        [ point: nextPoint(step["point"], goesTo), 
+          movement: goesTo ]
+    }
+
+    private nextPoint(point, goesTo) {
+        switch(goesTo) {
+            case "E": return [ point[0] + 1, point[1] ]
+            case "W": return [ point[0] - 1, point[1] ]
+            case "S": return [ point[0], point[1] + 1 ]
+            case "N": return [ point[0], point[1] - 1 ]
+        }
+    }
+
 }
 
 // Showing how to use the class
--- loopless-route-2/src/test/groovy/LoopesRouteTest.groovy
+++ loopless-route-2/src/test/groovy/LoopesRouteTest.groovy
@@ -13,7 +13,60 @@ class LoopesRouteTest extends GroovyTestCase {
     }
 
     @Test
-    public void test() {
-        
+    public void testEmptyRoute() {
+        assertEquals("", route.makeItLoopless(""))
+    }
+
+    @Test
+    public void testMoveToEastAndReturn() {
+        assertEquals("E", route.makeItLoopless("E"))
+        assertEquals("", route.makeItLoopless("EW"))
+        assertEquals("E", route.makeItLoopless("EEW"))
+        assertEquals("", route.makeItLoopless("EEWW"))
+        assertEquals("", route.makeItLoopless("EWEW"))
+        assertEquals("E", route.makeItLoopless("EEEWW"))
+    }
+
+    @Test
+    public void testMoveToWestAndReturn() {
+        assertEquals("W", route.makeItLoopless("W"))
+        assertEquals("", route.makeItLoopless("WE"))
+        assertEquals("W", route.makeItLoopless("WWE"))
+        assertEquals("", route.makeItLoopless("WWEE"))
+        assertEquals("", route.makeItLoopless("WEWE"))
+        assertEquals("W", route.makeItLoopless("WWWEE"))
+    }
+
+    @Test
+    public void testMoveToSouthAndReturn() {
+        assertEquals("S", route.makeItLoopless("S"))
+        assertEquals("", route.makeItLoopless("SN"))
+        assertEquals("S", route.makeItLoopless("SSN"))
+        assertEquals("", route.makeItLoopless("SSNN"))
+        assertEquals("", route.makeItLoopless("SNSN"))
+        assertEquals("S", route.makeItLoopless("SSSNN"))
+    }
+
+    @Test
+    public void testMoveToNorthAndReturn() {
+        assertEquals("N", route.makeItLoopless("N"))
+        assertEquals("", route.makeItLoopless("NS"))
+        assertEquals("N", route.makeItLoopless("NNS"))
+        assertEquals("", route.makeItLoopless("NNSS"))
+        assertEquals("", route.makeItLoopless("NSNS"))
+        assertEquals("N", route.makeItLoopless("NNNSS"))
+    }
+
+    @Test
+    public void testExampleTests() {
+        assertEquals("", route.makeItLoopless("SENW"))
+        assertEquals("SE", route.makeItLoopless("SEEW"))
+        assertEquals("", route.makeItLoopless("SEEESWWNNW"))
+        assertEquals("W", route.makeItLoopless("SWNNESESWNNESWW"))
+
+        //previous test which result was 'NN', now there is no loop, so it is the same
+        assertEquals("ESWWNNEEESSSWWWWNNNNEE", route.makeItLoopless("ESWWNNEEESSSWWWWNNNNEE"))
+        // previous solution would be 'NE', now only the loop is removed and the path is preserved
+        assertEquals("WNEE", route.makeItLoopless("WWSENNEE"))
     }
 }
