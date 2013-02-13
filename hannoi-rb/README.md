dad23fc4aa4bd82489a708295cd958c13f13db22
Ruben Chavarria <txingo@gmail.com>
2013-02-13 13:07:25 +0100

    Solved Hanoi Towers problem in Ruby

---
+++ hannoi-rb/README.md
@@ -0,0 +1,9 @@
+Darts, 501
+==========
+
+Solution to the solveet.com problem: [Hanoi towers](http://www.solveet.com/exercises/Torres-de-Hannoi/72)
+
+Run the solution with the command:
+
+	ruby hanoi.rb
+
---
+++ hannoi-rb/hanoi.rb
@@ -0,0 +1,14 @@
+#
+# Complete code at https://github.com/rchavarria/solveet-problems/tree/master/hanoi.rb
+#
+
+def hanoi(ring, origin, aux, target)
+    return if ring == 0
+
+    hanoi(ring - 1, origin, target, aux)
+    puts "Move ring (" + ring.to_s + "): " + origin + " -> " + target
+    hanoi(ring - 1, aux, origin, target)
+end
+
+# example of use
+hanoi(4, "[T1]", "[T2]", "[T3]")
