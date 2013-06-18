2ba39ae18e68794af831fe4325a9e33a53398018
Ruben Chavarria <txingo@gmail.com>
2013-06-18 08:38:28 +0200

    improved Adder class comments

--- add-positive-numbers-scala/Adder.scala
+++ add-positive-numbers-scala/Adder.scala
@@ -1,13 +1,17 @@
+/*
+ *  Complete solution and tests can be found at:
+ * https://github.com/rchavarria/solveet-problems/tree/master/add-positive-numbers-scala
+ */
 import scala.io.Source
 
 class Adder {
 
     def read(filePath: String): Int = {
         Source.fromFile(filePath)
-              .getLines         // obtiene todas las lineas
-              .drop(1)          // ignora la primera
-              .map(_.toInt)     // convierte numero de cada fila a entero
-              .filter(_ > 0)    // filtra los positivos
-              .sum              // suma
+              .getLines         // read all lines
+              .drop(1)          // ignore first one
+              .map(_.toInt)     // cast every line to integer
+              .filter(_ > 0)    // get only positive ones
+              .sum              // sum up
     }
 }
--- add-positive-numbers-scala/README.md
+++ add-positive-numbers-scala/README.md
@@ -2,7 +2,7 @@ solveet-add-positive-numbers
 ============================
 
 Solution to the solveet.com problem: [Add positive numbers](http://www.solveet.com/exercises/Suma-Positivos/25)
-written in the Scala language.
+written in the Scala programming language.
 
 The original problem is called `sizecon` and can be found in [spoj](http://www.spoj.pl/problems/SIZECON/)
 
