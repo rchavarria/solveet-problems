dc7765c5c1dd7a26fada6ba5fefbe144a08bb18e
Ruben Chavarria <txingo@gmail.com>
2013-07-05 13:59:11 +0200

    added a new test, still failing

--- spotify-best-before-scala/BestBeforeTest.scala
+++ spotify-best-before-scala/BestBeforeTest.scala
@@ -10,4 +10,8 @@ class BestBeforeTest extends FunSuite with ShouldMatchers {
         eat.bestBefore("1/1/1") should be( (1, "January", 2001) )
     }
 
+    test("best before '2/3/1' should be '3rd February 2001'") {
+        eat.bestBefore("2/3/1") should be( (3, "February", 2001) )
+    }
+
 }
