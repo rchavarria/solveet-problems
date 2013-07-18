461c72456a43b31e97c504a5d7f7020d5b740b6c
Ruben Chavarria <txingo@gmail.com>
2013-07-18 16:41:17 +0200

    solved. first I create a data structure with all permutations of the input data, then I create Date's for each one, and check if it is valid, then I choose the minimum Date and return it

--- spotify-best-before-scala/src/main/scala/BestBefore.scala
+++ spotify-best-before-scala/src/main/scala/BestBefore.scala
@@ -1,10 +1,84 @@
+import java.util.GregorianCalendar
+import java.util.Calendar
+
 /*
  *  Complete solution and tests can be found at:
  *      https://github.com/rchavarria/solveet-problems/tree/master/spotify-best-before-scala
  */
 class BestBefore {
-	
-	def bestBefore(input: String): (Int, String, Int) = {
-		(1, "January", 2001)
-	}
+
+    // the only public method
+    def bestBefore(input: String): (Int, String, Int) = {
+        val values = input.split("/")
+        val numbers = values map (_.toInt)
+
+        resultIs(minimumDateIn(possibleDates(builtFromPermutationsOf(numbers))))
+    }
+
+    private def builtFromPermutationsOf(numbers: Array[Int]) = {
+        val a = numbers(0)
+        val b = numbers(1)
+        val c = numbers(2)
+
+        List(
+            (a, b, c),
+            (a, c, b),
+            (b, a, c),
+            (b, c, a),
+            (c, a, b),
+            (c, b, a)
+            )
+    }
+
+    private def possibleDates(permutations: List[(Int, Int, Int)]) = {
+        val datesInMillis = permutations.map { dayMonthYear => 
+            val day = dayMonthYear._1
+            val month = dayMonthYear._2 - 1
+            val year = 2000 + dayMonthYear._3
+
+            val calendar = new GregorianCalendar
+            calendar.set(Calendar.DAY_OF_MONTH, day)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.YEAR, year)
+
+            // check if date is valid (equal as expected)
+            val invalidDate = calendar.get(Calendar.DAY_OF_MONTH) != day ||
+                              calendar.get(Calendar.MONTH) != month ||
+                              calendar.get(Calendar.YEAR) != year
+
+            // if not, return a really high value
+            if(invalidDate) Long.MaxValue else calendar.getTimeInMillis
+        }.filter(_ < Long.MaxValue)
+
+        if(datesInMillis.isEmpty) throw new Exception("No valid date can be found!")
+        datesInMillis
+    }
+
+    private def minimumDateIn(dates: List[Long]) = dates.min
+
+    private def resultIs(millis: Long) = {
+        val calendar = new GregorianCalendar
+        calendar.setTimeInMillis(millis)
+
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val month = MONTHS(calendar.get(Calendar.MONTH))
+        val year = calendar.get(Calendar.YEAR)
+
+        (day, month, year)
+    }
+
+    val MONTHS = List(
+                    "January", 
+                    "February", 
+                    "March", 
+                    "April", 
+                    "May", 
+                    "June", 
+                    "July", 
+                    "August", 
+                    "September", 
+                    "October", 
+                    "November", 
+                    "December"
+                    )
 }
--- spotify-best-before-scala/src/test/scala/BestBeforeSpec.scala
+++ spotify-best-before-scala/src/test/scala/BestBeforeSpec.scala
@@ -2,6 +2,8 @@ import org.specs2.mutable._
 
 class BestBeforeSpec extends Specification {
 
+  args.execute(sequential = true)
+
   "Best before method" should {
     val eat = new BestBefore
 
@@ -11,6 +13,18 @@ class BestBeforeSpec extends Specification {
 
     "return '3rd February 2001' when it is called with '1/2/3'" in {
       eat.bestBefore("1/2/3") must be equalTo( (3, "February", 2001) )
+    }
+
+    "return '4th February 2067' when it is called with '2/4/67'" in {
+      eat.bestBefore("2/4/67") must be equalTo( (4, "February", 2067) )
+    }
+
+    "return '13rd May 2032' when it is called with '5/32/13'" in {
+      eat.bestBefore("5/32/13") must be equalTo( (13, "May", 2032) )
+    }
+
+    "throw an exception when it is called with '31/9/73'" in {
+      eat.bestBefore("31/9/73") must throwAn[Exception]
     }
   }
 }
