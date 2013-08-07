81cb7eb50ac6e555707dfb512f58dba7e5d0c3a2
rchavarria <myemail@rchavarria.server>
2013-08-07 15:59:55 +0200

    merge sort algorithm solved in Scala programming language

--- merge-sort-scala/src/main/scala/MergeSort.scala
+++ merge-sort-scala/src/main/scala/MergeSort.scala
@@ -3,6 +3,8 @@
  *  Complete solution and tests can be found at:
  *      https://github.com/rchavarria/solveet-problems/tree/master/merge-sort-scala
  */
+import scala.collection.mutable
+
 class MergeSort {
 
     def sort(numbers: List[Int]): List[Int] = {
@@ -14,7 +16,23 @@ class MergeSort {
     }
 
     private def merge(left: List[Int], right: List[Int]): List[Int] = {
-    	val values = left ::: right
-    	List(values.min, values.max)
+        var i = 0 // index for left list
+        var j = 0 // index for right list
+        val values = mutable.ListBuffer[Int]() // results
+        
+        while(i < left.size || j < right.size) {
+            val leftValue = if(i < left.size) left(i) else Int.MaxValue
+            val rightValue = if(j < right.size) right(j) else Int.MaxValue
+
+            if(leftValue < rightValue){
+                i += 1
+                values += leftValue
+            } else {
+                j += 1
+                values += rightValue
+            }
+        }
+
+        values.toList
     }
 }
--- merge-sort-scala/src/test/scala/MergeSortSpec.scala
+++ merge-sort-scala/src/test/scala/MergeSortSpec.scala
@@ -23,6 +23,14 @@ class MergeSortSpec extends Specification {
       solveet.sort(List(2, 3, 1)) must be equalTo(List(1, 2, 3))
     }
 
+    "sort a list of more than three items" in {
+      solveet.sort(List(2, 3, 1, 4)) must be equalTo(List(1, 2, 3, 4))
+      solveet.sort(List(2, 3, 5, 1, 4)) must be equalTo(List(1, 2, 3, 4, 5))
+      solveet.sort(List(2, 5, 6, 3, 1, 4)) must be equalTo(List(1, 2, 3, 4, 5, 6))
+      solveet.sort(List(6, 5, 2, 3, 10, 8, 1, 9, 7, 4)) must be equalTo(List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+      solveet.sort(List(11, 10, 9, 8, 2, 3, 5, 6, 7, 1, 4)) must be equalTo(List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11))
+    }
+
   }
 
 }
