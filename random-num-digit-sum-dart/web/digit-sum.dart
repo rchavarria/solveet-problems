e6ecfb04ebc7f2aaa48c922ce31f920bce39e160
rchavarria <txingo@gmail.com>
2014-08-30 18:37:21 +0200

    test some edge cases

--- random-num-digit-sum-dart/web/digit-sum.dart
+++ random-num-digit-sum-dart/web/digit-sum.dart
@@ -1,6 +1,10 @@
 import "dart:io";
 
 void main() {
+  assertFalse(digitsSumTen(9), "Numbers below 10 are not allowed");
+  assertFalse(digitsSumTen(101), "Numbers greater than or equal to 100 are not allowed");
+  assertFalse(digitsSumTen(127), "Numbers greater than or equal to 100 are not allowed");
+  
   assertTrue(digitsSumTen(64), "64 digits sums 10");
   assertFalse(digitsSumTen(10), "10 digits doesn't sum 10");
   
