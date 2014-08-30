08c43c862322a9754182306b831aad7341c791a1
rchavarria <txingo@gmail.com>
2014-08-30 19:28:26 +0200

    refactor: rename public method

--- random-num-digit-sum-dart/web/digit-sum.dart
+++ random-num-digit-sum-dart/web/digit-sum.dart
@@ -4,18 +4,18 @@ void main() {
   NumberFilter filter = new NumberFilter();
 
   // edge cases
-  assertListsAreEqual(filter.filterNumbers([9]), [], "Numbers less than 10 are not allowed");
-  assertListsAreEqual(filter.filterNumbers([100]), [], "Numbers greater than or equal to 100 are not allowed");
-  assertListsAreEqual(filter.filterNumbers([127]), [], "Numbers greater than or equal to 100 are not allowed, even if all digits sum 10");
-  assertListsAreEqual(filter.filterNumbers([128]), [], "Numbers greater than or equal to 100 are not allowed, even if units and tenths sum 10");
+  assertListsAreEqual(filter.doFilter([9]), [], "Numbers less than 10 are not allowed");
+  assertListsAreEqual(filter.doFilter([100]), [], "Numbers greater than or equal to 100 are not allowed");
+  assertListsAreEqual(filter.doFilter([127]), [], "Numbers greater than or equal to 100 are not allowed, even if all digits sum 10");
+  assertListsAreEqual(filter.doFilter([128]), [], "Numbers greater than or equal to 100 are not allowed, even if units and tenths sum 10");
 
-  assertListsAreEqual(filter.filterNumbers([]), [], "Filter an empty List must return an empty List");
-  assertListsAreEqual(filter.filterNumbers([10]), [], "If all numbers whose digits don't sum 10, it must return empty list");
-  assertListsAreEqual(filter.filterNumbers([10, 20, 30]), [], "If all numbers whose digits don't sum 10, it must return empty list");
-  assertListsAreEqual(filter.filterNumbers([64]), [64], "If a number digits sums 10, it must be present in the returned array");
-  assertListsAreEqual(filter.filterNumbers([19, 28, 37]), [19, 28, 37], "It must return all numbers whose digits sum 10");
+  assertListsAreEqual(filter.doFilter([]), [], "Filter an empty List must return an empty List");
+  assertListsAreEqual(filter.doFilter([10]), [], "If all numbers whose digits don't sum 10, it must return empty list");
+  assertListsAreEqual(filter.doFilter([10, 20, 30]), [], "If all numbers whose digits don't sum 10, it must return empty list");
+  assertListsAreEqual(filter.doFilter([64]), [64], "If a number digits sums 10, it must be present in the returned array");
+  assertListsAreEqual(filter.doFilter([19, 28, 37]), [19, 28, 37], "It must return all numbers whose digits sum 10");
 
-  assertListsAreEqual(filter.filterNumbers([10, 19, 20, 28, 30, 37]), [19, 28, 37], "It must return all numbers whose digits sum 10");
+  assertListsAreEqual(filter.doFilter([10, 19, 20, 28, 30, 37]), [19, 28, 37], "It must return all numbers whose digits sum 10");
   
   stdout.writeln("Ok!");
 }
@@ -49,7 +49,7 @@ class NumberFilter {
     return (units + tenths) == 10;
   }
 
-  List filterNumbers(numbers) {
+  List doFilter(numbers) {
     List filtered = [];
     
     for (int i = 0; i < numbers.length; i++) {
