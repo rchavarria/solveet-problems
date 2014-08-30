c6f2c89f1e0b017321559127ea216ffc32e8cf8c
rchavarria <txingo@gmail.com>
2014-08-30 18:44:58 +0200

    implement a new function to check if two Lists are equal or not. Implement a function that filters an array of numbers (not finished yet)

--- random-num-digit-sum-dart/web/digit-sum.dart
+++ random-num-digit-sum-dart/web/digit-sum.dart
@@ -8,6 +8,10 @@ void main() {
   assertTrue(digitsSumTen(64), "64 digits sums 10");
   assertFalse(digitsSumTen(10), "10 digits doesn't sum 10");
   
+  assertListsAreEqual(filterNumbers([]), [], "Filter an empty List must return an empty List");
+  assertListsAreEqual(filterNumbers([10]), [], "If all numbers whose digits don't sum 10, it must return empty list");
+  assertListsAreEqual(filterNumbers([10, 20, 30]), [], "If all numbers whose digits don't sum 10, it must return empty list");
+  
   stdout.writeln("Ok!");
 }
 
@@ -21,9 +25,23 @@ void assertFalse(condition, msg) {
   assertTrue(!condition, msg);
 }
 
+void assertListsAreEqual(expected, actual, msg) {
+  if (expected.length != actual.length) {
+    throw new Exception(msg);
+  }
+  
+  for (int i = 0; i < expected.length; i++) {
+    assertTrue(expected[i] == actual[i], msg);
+  }
+}
+
 bool digitsSumTen(number) {
   int units = number % 10;
   int tenths = (number ~/ 10) % 10;
   
   return (units + tenths) == 10;
+}
+
+List filterNumbers(unfilteredNumbers) {
+  return [];  
 }
