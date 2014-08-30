ac954d09ea0217c56f982e87754613ef00e058fa
rchavarria <txingo@gmail.com>
2014-08-30 18:36:27 +0200

    make the first two examples pass

--- random-num-digit-sum-dart/web/digit-sum.dart
+++ random-num-digit-sum-dart/web/digit-sum.dart
@@ -1,10 +1,25 @@
+import "dart:io";
 
 void main() {
-  assertTrue(false, "Must be true");
+  assertTrue(digitsSumTen(64), "64 digits sums 10");
+  assertFalse(digitsSumTen(10), "10 digits doesn't sum 10");
+  
+  stdout.writeln("Ok!");
 }
 
 void assertTrue(condition, msg) {
   if (!condition) {
     throw new Exception(msg);
   }
+}
+
+void assertFalse(condition, msg) {
+  assertTrue(!condition, msg);
+}
+
+bool digitsSumTen(number) {
+  int units = number % 10;
+  int tenths = (number ~/ 10) % 10;
+  
+  return (units + tenths) == 10;
 }
