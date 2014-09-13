64e35693bd5994b31a7f09c11fb0bd37bc13584e
rchavarria <txingo@gmail.com>
2014-09-13 19:29:00 +0200

    Red: last case, when the game is a draw

--- differences-game-dart/web/differences-game.dart
+++ differences-game-dart/web/differences-game.dart
@@ -12,7 +12,8 @@ void main() {
   playWithPredefinedPlayers('Dave', [Choices.HEADS, Choices.HEADS, Choices.TAILS], 'Dave must win');
   playWithPredefinedPlayers('John', [Choices.HEADS, Choices.TAILS, Choices.HEADS], 'John must win');
   playWithPredefinedPlayers('Jane', [Choices.TAILS, Choices.HEADS, Choices.HEADS], 'Jane must win');
-  
+  playWithPredefinedPlayers('draw', [Choices.HEADS, Choices.HEADS, Choices.HEADS], 'The game is a draw');
+    
   stdout.writeln('Ok!');
 }
 
