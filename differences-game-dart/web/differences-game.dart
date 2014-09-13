64206a77ce870447c47e413ce6d80b4812f62ab4
rchavarria <txingo@gmail.com>
2014-09-13 18:58:14 +0200

    Red: first test starting from the last player

--- differences-game-dart/web/differences-game.dart
+++ differences-game-dart/web/differences-game.dart
@@ -3,6 +3,8 @@ import "assert.dart";
 
 void main() {
   Asserts assertThat = new Asserts();
+
+  assertThat.areEqual('Dave', play( ['Jane', 'John', 'Dave'], ['tails', 'tails', 'heads']));
   
   stdout.writeln('Ok!');
 }
