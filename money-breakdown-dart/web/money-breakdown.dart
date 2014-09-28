822020eb3e35487515c0153eb8b534de21edbd29
rchavarria <txingo@gmail.com>
2014-09-28 18:24:36 +0200

    Red: 2€ breaks down as '1 coin of 2€'

--- money-breakdown-dart/web/money-breakdown.dart
+++ money-breakdown-dart/web/money-breakdown.dart
@@ -5,6 +5,10 @@ main() {
   test('1€ breaks down as a 1 coin of 1€', () {
     expect(breakdown(1), equals('1 coin of 1€'));
   });
+  
+  test('2€ breaks down as a 1 coin of 2€', () {
+    expect(breakdown(2), equals('1 coin of 2€'));
+  });
 
 }
 
