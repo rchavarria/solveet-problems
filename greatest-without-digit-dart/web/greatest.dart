f4c25aa855e3678e2c85396138b1d406e77b5912
rchavarria <txingo@gmail.com>
2014-10-08 00:31:54 +0200

    first test : 0 is less than 1 without 1

--- greatest-without-digit-dart/web/greatest.dart
+++ greatest-without-digit-dart/web/greatest.dart
@@ -1,2 +1,15 @@
-main() {
+import 'package:unittest/unittest.dart';
+
+void main() {
+  group('Greatest Z less than X without digit Y', () {
+    test('0 is less than 1 without 1', () {
+      int z = lessThan(1, 1);
+      expect(0, equals(z));
+    });
+  });
 }
+
+int lessThan(x, y) {
+  return 0;
+}
+
