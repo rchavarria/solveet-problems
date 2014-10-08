d4ec5191923ca2d2e45446dad76bad66580add05
rchavarria <txingo@gmail.com>
2014-10-09 00:22:54 +0200

    Red: 1 is less than 2 without 2

--- greatest-without-digit-dart/web/greatest.dart
+++ greatest-without-digit-dart/web/greatest.dart
@@ -1,16 +1,26 @@
 import 'package:unittest/unittest.dart';
 
 void main() {
-  group('Greatest Z less than X without digit Y', () {
+  group('Greatest Z less than X without digit Y:', () {
 
-    group('Numbers less than 10', () {
+    group('for numbers less than 10,', () {
 
       test('0 is less than 1 without 1', () {
         int z = lessThan(1, 1);
         expect(0, equals(z));
-      }
+      });
 
-    }
+      test('1 is less than 2 without 2', () {
+        int z = lessThan(2, 2);
+        expect(1, equals(z));
+      });
+
+    });
+
   });
+}
+
+int lessThan(x, y) {
+  return 0;
 }
 
