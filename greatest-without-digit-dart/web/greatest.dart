74507a736f47751e8c2a6e3feac13244cf91bd85
rchavarria <txingo@gmail.com>
2014-10-09 00:03:44 +0200

    Red: 0 is less than 1 without 1

--- greatest-without-digit-dart/web/greatest.dart
+++ greatest-without-digit-dart/web/greatest.dart
@@ -3,6 +3,14 @@ import 'package:unittest/unittest.dart';
 void main() {
   group('Greatest Z less than X without digit Y', () {
 
+    group('Numbers less than 10', () {
+
+      test('0 is less than 1 without 1', () {
+        int z = lessThan(1, 1);
+        expect(0, equals(z));
+      }
+
+    }
   });
 }
 
