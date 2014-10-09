b487c8ec1be88b9e7b287936df1060ddd7f2ec4e
rchavarria <txingo@gmail.com>
2014-10-10 00:45:40 +0200

    Add test for numbers with 3 digits

--- greatest-without-digit-dart/web/greatest.dart
+++ greatest-without-digit-dart/web/greatest.dart
@@ -7,37 +7,52 @@ void main() {
 
       test('0 is less than 1 without 1', () {
         int z = lessThan(1, 1);
-        expect(0, equals(z));
+        expect(z, equals(0));
       });
 
       test('1 is less than 2 without 2', () {
         int z = lessThan(2, 2);
-        expect(1, equals(z));
+        expect(z, equals(1));
       });
 
       test('0 is less than 2 without 1', () {
         int z = lessThan(2, 1);
-        expect(0, equals(z));
+        expect(z, equals(0));
       });
 
       test('2 is less than 3 without 1', () {
         int z = lessThan(3, 1);
-	expect(2, equals(z));
+	expect(z, equals(2));
       });
 
       test('9 is less than 11 without 1', () {
         int z = lessThan(11, 1);
-	expect(9, equals(z));
+	expect(z, equals(9));
       });
 
       test('19 is less than 29 without 2', () {
         int z = lessThan(29, 2);
-	expect(19, equals(z));
+	expect(z, equals(19));
       });
 
       test('26 is less than 28 without 7', () {
         int z = lessThan(28, 7);
-	expect(26, equals(z));
+	expect(z, equals(26));
+      });
+
+      test('467 is less than 469 without 8', () {
+        int z = lessThan(469, 8);
+	expect(z, equals(467));
+      });
+
+      test('459 is less than 469 without 6', () {
+        int z = lessThan(469, 6);
+	expect(z, equals(459));
+      });
+
+      test('399 is less than 469 without 4', () {
+        int z = lessThan(469, 4);
+	expect(z, equals(399));
       });
 
     });
