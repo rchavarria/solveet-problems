bb212dc7871f57008a6ec9153b8b6da13041d1da
rchavarria <txingo@gmail.com>
2014-10-18 22:48:56 +0200

    Red: mixes the two first characters of the first phrase

--- phrase-mixer-dart/web/mixer.dart
+++ phrase-mixer-dart/web/mixer.dart
@@ -2,7 +2,11 @@ import 'package:unittest/unittest.dart';
 
 void main() {
   group('Phrase mixer', () {
-
+    
+    test('Mixes the two first characters of the first phrase', () {
+      String mixed = mix('mi', '');
+      expect(mixed, equals('mi'));
+    }
   });
 }
 
