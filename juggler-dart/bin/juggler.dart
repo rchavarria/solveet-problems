a15a45ab2aac9fe5465f2aec9a8350b808281737
rchavarria <txingo@gmail.com>
2015-01-05 16:40:58 +0100

    Green: first test. Returns empty list form numbers less than 1

--- juggler-dart/bin/juggler.dart
+++ juggler-dart/bin/juggler.dart
@@ -8,5 +8,21 @@ void main() {
     });
     
   });
+
+  group('Juggler sequence', () {
+
+      test('for numbers less than 1', () {
+          Juggler juggler = new Juggler();
+          List<int> sequence = juggler.sequence(0);
+          expect([], sequence);
+      });
+
+  });
+}
+
+class Juggler {
+    List<int> sequence(int N) {
+        return [];
+    }
 }
 
