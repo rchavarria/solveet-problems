7109e735c6ec76834fcd0bcc9efb8a32743ce2f7
rchavarria <txingo@gmail.com>
2015-01-05 17:00:08 +0100

    Refactor: remove duplication

--- juggler-dart/bin/juggler.dart
+++ juggler-dart/bin/juggler.dart
@@ -4,22 +4,20 @@ import 'dart:math';
 void main() {
   group('Juggler sequence', () {
 
+      Juggler juggler = new Juggler();
+
       test('is empty  for numbers less than 1', () {
-          Juggler juggler = new Juggler();
           List<int> sequence = juggler.sequence(0);
           expect([], sequence);
       });
 
       test('returns N as the first number in it', () {
-          Juggler juggler = new Juggler();
-
           expect(1, juggler.sequence(1)[0]);
           expect(2, juggler.sequence(2)[0]);
           expect(5, juggler.sequence(5)[0]);
       });
 
       test('returns N square root as second number if N is even', () {
-          Juggler juggler = new Juggler();
           expect(4, juggler.sequence(16)[1]);
       });
   });
