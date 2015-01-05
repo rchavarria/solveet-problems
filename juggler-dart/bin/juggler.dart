c0c344b15364e943105c288cfd7378af82690874
rchavarria <txingo@gmail.com>
2015-01-05 16:45:17 +0100

    Red: the first number in the sequence is N itself

--- juggler-dart/bin/juggler.dart
+++ juggler-dart/bin/juggler.dart
@@ -3,10 +3,16 @@ import 'package:unittest/unittest.dart';
 void main() {
   group('Juggler sequence', () {
 
-      test('is emptyis empty  for numbers less than 1', () {
+      test('is empty  for numbers less than 1', () {
           Juggler juggler = new Juggler();
           List<int> sequence = juggler.sequence(0);
           expect([], sequence);
+      });
+
+      test('is 1 for number 1', () {
+          Juggler juggler = new Juggler();
+          List<int> sequence = juggler.sequence(1);
+          expect([1], sequence);
       });
 
   });
