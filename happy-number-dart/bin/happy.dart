ac451378ba589cdaf73dc6837a07857b201d390a
rchavarria <txingo@gmail.com>
2015-01-06 01:41:31 +0100

    Green (with more examples added)

--- happy-number-dart/bin/happy.dart
+++ happy-number-dart/bin/happy.dart
@@ -9,9 +9,17 @@ void main() {
             test('splits a number below 10 in an list with one element', () {
                 HappyNumbers happy = new HappyNumbers();
                 expect(1, happy.digits(1).length);
+                expect(1, happy.digits(3).length);
+                expect(1, happy.digits(9).length);
             });
+
         });
 
     });
 }
 
+class HappyNumbers {
+    List<int> digits(number) {
+        return [number];
+    }
+}
