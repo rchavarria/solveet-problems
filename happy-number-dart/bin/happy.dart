1a15f7dc3444da444ca08bab86347b45e1a02a01
rchavarria <txingo@gmail.com>
2015-01-06 01:43:37 +0100

    Refactor: remove duplication

--- happy-number-dart/bin/happy.dart
+++ happy-number-dart/bin/happy.dart
@@ -4,17 +4,17 @@ import 'dart:math';
 void main() {
     group('Happy numbers', () {
 
+        HappyNumbers happy = new HappyNumbers();
+
         group('#digits', () {
 
             test('splits a number below 10 in an list with one element', () {
-                HappyNumbers happy = new HappyNumbers();
                 expect(1, happy.digits(1).length);
                 expect(1, happy.digits(3).length);
                 expect(1, happy.digits(9).length);
             });
 
             test('returns the same number if it is below 10', () {
-                HappyNumbers happy = new HappyNumbers();
                 expect([2], happy.digits(2));
             });
 
