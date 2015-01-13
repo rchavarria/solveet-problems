f49e29fefd148c7f59a768ba27a950a7ef7fbb94
rchavarria <txingo@gmail.com>
2015-01-14 00:00:52 +0100

    First attemp to the API. It needs an API Key, private Key,... so you get a HTTP 409 Forbiddenç

--- marvel-api-dart/bin/marvel.dart
+++ marvel-api-dart/bin/marvel.dart
@@ -1,4 +1,5 @@
 import 'package:unittest/unittest.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
     group('Foo bar', () {
@@ -7,6 +8,15 @@ void main() {
             expect(1, 0 + 1);
         });
 
+        test('gets text from a URL', () {
+            String baseEndpoint = 'http://gateway.marvel.com';
+            String url = '${baseEndpoint}/v1/public/comics';
+
+            http.get(url)
+                .then((response) {
+                    print(response.statusCode);
+                });
+        });
+
     });
 }
-
