b67f023fdb45545fa097f143a84f25ad5572f437
rchavarria <txingo@gmail.com>
2015-01-14 09:45:45 +0100

    Calculate some kind of MD5 digest

--- marvel-api-dart/bin/marvel.dart
+++ marvel-api-dart/bin/marvel.dart
@@ -1,10 +1,14 @@
 import 'package:unittest/unittest.dart';
+import 'package:cipher/cipher.dart';
+import 'package:cipher/impl/server.dart';
+import 'dart:typed_data';
 
 // package http can be used to get remote content from the server
 // documentation: https://pub.dartlang.org/packages/http
 import 'package:http/http.dart' as http;
 
 void main() {
+
     group('Foo bar', () {
 
         test('tests', () {
@@ -19,6 +23,15 @@ void main() {
                 .then((response) {
                     print(response.statusCode);
                 });
+        });
+
+        test('calculating an MD5 digest with cipher package', () {
+            initCipher();
+
+            // Example from: https://github.com/izaera/cipher/wiki/Using-digests
+            var digest = new Digest('MD5');
+            var digestValue = digest.process(new Uint8List.fromList('compute md5 of this'.codeUnits));
+            print('Digest value is: ${digestValue}');
         });
 
     });
