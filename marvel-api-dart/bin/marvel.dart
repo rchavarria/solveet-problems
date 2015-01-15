1a2bbcca0a5834a54c6d5381f9bac4df76c5dd23
rchavarria <txingo@gmail.com>
2015-01-15 01:12:02 +0100

    Take a string, converts to utf8, md5'ed it, convert to hexadecimal values, and print it

--- marvel-api-dart/bin/marvel.dart
+++ marvel-api-dart/bin/marvel.dart
@@ -2,6 +2,7 @@ import 'package:unittest/unittest.dart';
 import 'package:cipher/cipher.dart';
 import 'package:cipher/impl/server.dart';
 import 'dart:typed_data';
+import 'dart:convert';
 
 // package http can be used to get remote content from the server
 // documentation: https://pub.dartlang.org/packages/http
@@ -28,10 +29,20 @@ void main() {
         test('calculating an MD5 digest with cipher package', () {
             initCipher();
 
+            String inputString = 'compute md5 of this';
+            Utf8Encoder encoder = new Utf8Encoder();
+            List<int> utf8Data = encoder.convert(inputString);
+            Utf8List inputData = new Uint8List.fromList(utf8Data);
+
             // Example from: https://github.com/izaera/cipher/wiki/Using-digests
-            var digest = new Digest('MD5');
-            var digestValue = digest.process(new Uint8List.fromList('compute md5 of this'.codeUnits));
-            print('Digest value is: ${digestValue}');
+            Digest digest = new Digest('MD5');
+            Uint8List digestValue = digest.process(inputData);
+            // convert List<int> into a string representation of hexadecimal
+            // values (this is how MD5 digests are usually represented)
+            List<String> output = digestValue.map((i) {
+               return i.toRadixString(16).padLeft(2, '0');
+            });
+            print('Digest value is: ${output}');
         });
 
     });
