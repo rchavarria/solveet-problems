ecbf577127eabfdb11caa1391e80d36cb786d60c
rchavarria <txingo@gmail.com>
2015-01-15 23:57:10 +0100

    Split read_key_file into two files: test and production code

--- marvel-api-dart/bin/read_key_file.dart
+++ marvel-api-dart/bin/read_key_file.dart
@@ -1,28 +1,6 @@
-import 'package:unittest/unittest.dart';
 import 'dart:convert';
 import 'dart:io';
 import 'dart:async';
-
-void main(List<String> arguments) {
-
-    group('KeyFileReader Spike:', () {
-
-        test('reads a .env file with and sets private key', () {
-            var reader = new KeyFileReader('bin/.env_test');
-            reader.read();
-            expect('private key value', reader.privateKey);
-        });
-
-        test('reads a .env file with and sets public key', () {
-            var reader = new KeyFileReader('bin/.env_test');
-            reader.read();
-            expect('public key value', reader.publicKey);
-        });
-
-    });
-
-}
-
 
 class KeyFileReader {
 
