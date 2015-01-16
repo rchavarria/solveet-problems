e190dad3872d97f5076d65f8196696497892e771
rchavarria <txingo@gmail.com>
2015-01-16 21:59:40 +0100

    Remove duplicated code

--- marvel-api-dart/bin/marvel_api.dart
+++ marvel-api-dart/bin/marvel_api.dart
@@ -18,25 +18,14 @@ class MarvelApi {
     }
 
     Future authenticate() {
-        var authenticationToken = timestamp.toString() + privateKey + publicKey;
-        var hash = md5.digest(authenticationToken);
-
-        // making the first api call to authenticate
-        String baseEndpoint = 'http://gateway.marvel.com';
-        String query = [
-            'ts=${timestamp}',
-            'apikey=${publicKey}',
-            'hash=${hash}'].join('&');
-        String url = '${baseEndpoint}/v1/public/comics?${query}';
-
-        return http.get(url)
+        return makeApiRequest('comics')
             .then((response) {
                 var success = response.statusCode == 200;
                 return new Future.value(success);
             });
     }
 
-    Future comics() {
+    Future makeApiRequest(entity) {
         timestamp += 1;
         var authenticationToken = timestamp.toString() + privateKey + publicKey;
         var hash = md5.digest(authenticationToken);
@@ -47,9 +36,13 @@ class MarvelApi {
             'ts=${timestamp}',
             'apikey=${publicKey}',
             'hash=${hash}'].join('&');
-        String url = '${baseEndpoint}/v1/public/comics?${query}';
+        String url = '${baseEndpoint}/v1/public/${entity}?${query}';
 
-        return http.get(url)
+        return http.get(url);
+    }
+
+    Future comics() {
+        return makeApiRequest('comics')
             .then((response) {
                 var result = JSON.decode(response.body);
                 var comics = result['data'];
