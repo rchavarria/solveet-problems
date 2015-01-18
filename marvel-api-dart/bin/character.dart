bb0d2008614efc6a91088c33fb5b81dc9737ea0f
rchavarria <txingo@gmail.com>
2015-01-18 17:59:00 +0100

    Add comments

--- marvel-api-dart/bin/character.dart
+++ marvel-api-dart/bin/character.dart
@@ -1,12 +1,19 @@
 import 'image_url_builder.dart';
 
+/**
+  * Represents a Marvel character
+  */
 class Character {
     var builder = new ImageUrlBuilder();
     String id;
     String name;
     String description;
+    /* URL to an image of the character */
     String thumbnailUrl;
 
+    /**
+      * Named constructor to make it easy to parse data from Marvel API
+      */
     Character.fromJson(Map json) {
         id = json['id'];
         name = json['name'];
--- marvel-api-dart/bin/comic.dart
+++ marvel-api-dart/bin/comic.dart
@@ -1,12 +1,19 @@
 import 'image_url_builder.dart';
 
+/**
+  * Represents a Marvel comic
+  */
 class Comic {
     var builder = new ImageUrlBuilder();
     String id;
     String title;
     String description;
+    /* URL to an image of the comic */
     String thumbnailUrl;
 
+    /**
+      * Named constructor to make it easy to parse data from Marvel API
+      */
     Comic.fromJson(Map json) {
         id = json['id'];
         title = json['title'];
