530a3e190d5578638279530966870253fe49484a
rchavarria <txingo@gmail.com>
2015-01-18 18:04:07 +0100

    Copy last changes of external classes

--- marvel-api-dart/test/example.dart
+++ marvel-api-dart/test/example.dart
@@ -1,6 +1,6 @@
-import 'dart:typed_data';
-import 'dart:async';
-import 'dart:convert';
+import 'dart:typed_data'; // algunos tipos necesarios para calcular MD5
+import 'dart:async'; // soporte para Futures
+import 'dart:convert'; // soporte para JSON
 
 // paquete de cifrado, incluye clase para calcular MD5
 import 'package:cipher/cipher.dart';
@@ -8,6 +8,10 @@ import 'package:cipher/impl/server.dart';
 // paquete para realizar peticiones HTTP en una aplicación de consola
 import 'package:http/http.dart' as http;
 
+
+/**
+  * Ejecución del programa principal
+  */
 void main() {
     print('Recuperando comics y personajes de Marvel');
 
@@ -17,6 +21,7 @@ void main() {
     var md5 = new Utf8String2MD5();
     var api = new MarvelApi(md5, privateKey, publicKey);
 
+    // el primer paso es autenticarse contra Marvel
     api.authenticate().then((authenticated) {
         if (!authenticated) {
             print('Error, marvel no pudo autentificarte, ¿has usado las claves adecuadas?');
@@ -29,7 +34,7 @@ void main() {
             comics.forEach((c) => print('- ${c.title}'));
         });
 
-        // consulta la lista de persojanes e imprime su nombre
+        // consulta la lista de personajes e imprime su nombre
         api.characters().then((characters) {
             print('Personajes:');
             characters.forEach((c) => print('- ${c.name}'));
@@ -161,47 +166,61 @@ class MarvelApi {
 
 }
 
+/**
+  * Represents a Marvel comic
+  */
 class Comic {
+    var builder = new ImageUrlBuilder();
     String id;
     String title;
     String description;
+    /* URL to an image of the comic */
     String thumbnailUrl;
 
+    /**
+      * Named constructor to make it easy to parse data from Marvel API
+      */
     Comic.fromJson(Map json) {
         id = json['id'];
         title = json['title'];
         description = json['description'];
-        thumbnailUrl = buildThumbnailUrl(json);
-    }
-
-    String buildThumbnailUrl(Map json) {
-        Map thumbnail = json['thumbnail'];
-        if (thumbnail == null) {
-            return '';
-        }
-
-        var path = thumbnail['path'];
-        var extension = thumbnail['extension'];
-        return '${path}.${extension}';
+        thumbnailUrl = builder.fromMap(json['thumbnail']);
     }
 
 }
 
+/**
+  * Represents a Marvel character
+  */
 class Character {
+    var builder = new ImageUrlBuilder();
     String id;
     String name;
     String description;
+    /* URL to an image of the character */
     String thumbnailUrl;
 
+    /**
+      * Named constructor to make it easy to parse data from Marvel API
+      */
     Character.fromJson(Map json) {
         id = json['id'];
         name = json['name'];
         description = json['description'];
-        thumbnailUrl = buildThumbnailUrl(json);
+        thumbnailUrl = builder.fromMap(json['thumbnail']);
     }
 
-    String buildThumbnailUrl(Map json) {
-        Map thumbnail = json['thumbnail'];
+}
+
+/**
+  * Handles Image entities
+  */
+class ImageUrlBuilder {
+
+    /**
+      * Builds an Image URL from a map returned by the API
+      */
+    String fromMap(Map thumbnail) {
         if (thumbnail == null) {
             return '';
         }
@@ -210,5 +229,5 @@ class Character {
         var extension = thumbnail['extension'];
         return '${path}.${extension}';
     }
-}
 
+}
