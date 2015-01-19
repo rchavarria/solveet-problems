c78307e8a3bc8108f1f9ac3be6d6c3075a9364a2
rchavarria <txingo@gmail.com>
2015-01-19 22:35:42 +0100

    Write more comprehensive comments

--- marvel-api-dart/bin/string2md5.dart
+++ marvel-api-dart/bin/string2md5.dart
@@ -1,31 +1,37 @@
+// set `cipher` as a project's dependency
 import 'package:cipher/cipher.dart';
 import 'package:cipher/impl/server.dart';
 import 'dart:typed_data';
 import 'dart:convert';
 
 /**
-  * Utility class that encapsulates the process of calculating a MD5 hash of a
-  * given message
-  */
+ * Uses cipher package to compute the MD5 hash of a given String
+ */
 class Utf8String2MD5 {
 
     Utf8Encoder encoder;
-    Digest md5Digest;
+    Digest md5;
 
     Utf8String2MD5() {
+        // cipher must be initialized before using any class from the package
         initCipher();
 
+        // cipher class to compute the MD5
+        md5 = new Digest('MD5');
         encoder = new Utf8Encoder();
-        md5Digest = new Digest('MD5');
     }
 
     /**
-      * Computes the MD5 has of `message`
-      */
+     * @param message Message which MD5 hash will be computed
+     * @return the MD5 hash of `message`
+     */
     String digest(String message) {
+        // get utf-8 values of the message
         List<int> utf8Data = encoder.convert(message);
+        // convert to the input data for the cipher class
         Utf8List inputData = new Uint8List.fromList(utf8Data);
-        Uint8List digestValue = md5Digest.process(inputData);
+        // compute the MD5
+        Uint8List digestValue = md5.process(inputData);
 
         return digestValue
             .map((i) => i.toRadixString(16)) // converts to hexadecimal string
