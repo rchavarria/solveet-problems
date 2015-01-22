d495bb39ec6f017fd21c108ffd005a602e76f3ad
rchavarria <txingo@gmail.com>
2015-01-22 17:12:48 +0100

    Refactor: remove magical numbers

--- next-leap-year-dart/bin/next_leap_year.dart
+++ next-leap-year-dart/bin/next_leap_year.dart
@@ -1,4 +1,6 @@
 class NextLeapYear {
+    static int MINIMUM_LEAP_INTERVAL = 4;
+    static int LEAP_YEARS_INTERVAL_EXCEPTION = 100;
     int year;
 
     NextLeapYear(this.year);
@@ -9,26 +11,25 @@ class NextLeapYear {
         }
 
         int next = findGoodCandidate(year);
-
         return new NextLeapYear(next);
     }
 
-    int findGoodCandidate(startingAtYear) {
-        int differenceToLeap = 4 - (startingAtYear % 4);
-        int next = year + differenceToLeap;
-        if (!isLeap(next)) {
-            next += 4;
-        }
+    int findGoodCandidate(startingAt) {
+        int differenceToLeap = MINIMUM_LEAP_INTERVAL - (startingAt % MINIMUM_LEAP_INTERVAL);
+        int next = startingAt + differenceToLeap;
 
-        return next;
+        if (isLeap(next)) {
+            return next;
+        }
+        return findGoodCandidate(next);
     }
 
     boolean isLeap(candidateYear) {
-        if (candidateYear % 100 == 0) {
+        if (candidateYear % LEAP_YEARS_INTERVAL_EXCEPTION == 0) {
             return false;
         }
 
-        return candidateYear % 4 == 0;
+        return candidateYear % MINIMUM_LEAP_INTERVAL == 0;
     }
 
 }
