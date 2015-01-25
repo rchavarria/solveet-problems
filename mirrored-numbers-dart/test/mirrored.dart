2a1d582c364e242cd5c2c33beef3decf9b7ca1ed
rchavarria <txingo@gmail.com>
2015-01-25 11:26:39 +0100

    Use new method to compute several equilibrated numbers

--- mirrored-numbers-dart/test/mirrored.dart
+++ mirrored-numbers-dart/test/mirrored.dart
@@ -4,6 +4,10 @@ void main() {
     var intReversed = reverseNumber(number);
     
     print('sum: ${number + intReversed}');
+    [1, 2, 10, 15, 123, 321, 1234, 7890].forEach((number) {
+        var equilibrated = getEquilibratedNumber(number);
+        print('${number} + <mirrored>: ${equilibrated}');
+    });
 }
 
 int getEquilibratedNumber(int number) => number + reverseNumber(number);
