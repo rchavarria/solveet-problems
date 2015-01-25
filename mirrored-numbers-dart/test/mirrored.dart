da5f4b08fe4827b29f6ffe17be05bbe8eceb08cb
rchavarria <txingo@gmail.com>
2015-01-25 11:21:14 +0100

    Use extracted method

--- mirrored-numbers-dart/test/mirrored.dart
+++ mirrored-numbers-dart/test/mirrored.dart
@@ -1,17 +1,7 @@
 void main() {
     print('Start');
     var number = 123;
-    var strNumber = number.toString();
-    print('strNumber: ${strNumber}');
-    var digits = strNumber.split('');
-    print('digits: ${digits}');
-    var reversed = [];
-    digits.forEach((d) => reversed.insert(0, d));
-    print('reversed: ${reversed}');
-    var strReversed = reversed.join();
-    print ('strReversed: ${strReversed}');
-    var intReversed = int.parse(strReversed);
-    print ('intReversed: ${intReversed}');
+    var intReversed = reverseNumber(number);
     
     print('sum: ${number + intReversed}');
 }
