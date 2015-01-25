1f18d34464a9d0da82b94deda0318677daa0b83f
rchavarria <txingo@gmail.com>
2015-01-25 11:05:55 +0100

    Start writing some code to experiment with List class methods

---
+++ mirrored-numbers-dart/test/mirrored.dart
@@ -0,0 +1,14 @@
+void main() {
+    print('Start');
+    var number = 123;
+    var strNumber = number.toString();
+    print('strNumber: ${strNumber}');
+    var digits = strNumber.split('');
+    print('digits: ${digits}');
+    var reversed = [];
+    digits.forEach((d) => reversed.insert(0, d));
+    print('reversed: ${reversed}');
+    var strReversed = reversed.join();
+    print ('strReversed: ${strReversed}');
+
+}
