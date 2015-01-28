ad439c86a9a8ff7f551c76a0aad72b10b93bc912
rchavarria <txingo@gmail.com>
2015-01-28 09:14:12 +0100

    Create hook up test

---
+++ stone-paper-scissors-dart/test/stone_paper_scissors.dart
@@ -0,0 +1,14 @@
+import 'package:unittest/unittest.dart';
+
+void main() {
+
+    group('Hook up', () {
+
+        test('hooks up', () {
+            expect(2 + 2, equals(4));
+        });
+
+    });
+
+}
+
