ee62472b764dd1ffd0f55282bda7d54fef3cb582
rchavarria <txingo@gmail.com>
2015-01-29 17:42:50 +0100

    New test : paper wins stone

--- stone-paper-scissors-dart/bin/stone_paper_scissors.dart
+++ stone-paper-scissors-dart/bin/stone_paper_scissors.dart
@@ -2,6 +2,10 @@
 class Game {
 
     String play(String choice1, String choice2) {
+        if (choice1 == 'paper') {
+            return 'Player 1';
+        }
+
         return 'draw';
     }
 
--- stone-paper-scissors-dart/test/stone_paper_scissors.dart
+++ stone-paper-scissors-dart/test/stone_paper_scissors.dart
@@ -10,6 +10,13 @@ void main() {
             var gameResult = game.play('stone', 'stone');
             expect(gameResult, equals('draw'));
         });
+
+        test('paper wins stone', () {
+            var game = new Game();
+            var gameResult = game.play('paper', 'stone');
+            expect(gameResult, equals('Player 1'));
+        });
+
     });
 
 }
