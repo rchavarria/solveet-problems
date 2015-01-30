58c0affa2f51cea6693dd9bd1d82ef9b74b6d929
rchavarria <txingo@gmail.com>
2015-01-30 09:37:57 +0100

    Refactor : move logic from switch to data structure (paper)

--- stone-paper-scissors-dart/bin/stone_paper_scissors.dart
+++ stone-paper-scissors-dart/bin/stone_paper_scissors.dart
@@ -11,17 +11,19 @@ class Game {
                 'stone': 'Player 2',
                 'paper': 'Player 1',
                 'scissors': 'draw'
-            }
+            },
+            'paper': {
+                'stone': 'Player 1',
+                'paper': 'draw',
+                'scissors': 'Player 2'
+             }
         };
 
         switch(choice1) {
         case 'scissors':
             return rulesMap[choice1][choice2];
         case 'paper':
-            if (choice2 == 'scissors') {
-                return 'Player 2';
-            }
-            break;
+            return rulesMap[choice1][choice2];
         case 'stone':
             if (choice2 == 'paper') {
                 return 'Player 2';
