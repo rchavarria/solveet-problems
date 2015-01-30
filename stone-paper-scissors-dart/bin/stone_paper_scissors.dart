8a6b5fde62f6f883e694a78bf4e37a05cc4e1124
rchavarria <txingo@gmail.com>
2015-01-30 09:43:08 +0100

    Only use data structure to solve the problem

--- stone-paper-scissors-dart/bin/stone_paper_scissors.dart
+++ stone-paper-scissors-dart/bin/stone_paper_scissors.dart
@@ -1,29 +1,25 @@
 
 class Game {
 
-    String play(String choice1, String choice2) {
-        if (choice1 == choice2) {
-            return 'draw';
+    var rulesMap = {
+        'stone': {
+            'stone': 'draw',
+            'paper': 'Player 2',
+            'scissors': 'Player 1'
+        },
+        'paper': {
+            'stone': 'Player 1',
+            'paper': 'draw',
+            'scissors': 'Player 2'
+        },
+        'scissors': {
+            'stone': 'Player 2',
+            'paper': 'Player 1',
+            'scissors': 'draw'
         }
+    };
 
-        var rulesMap = {
-            'stone': {
-                'stone': 'draw',
-                'paper': 'Player 2',
-                'scissors': 'Player 1'
-            },
-            'paper': {
-                'stone': 'Player 1',
-                'paper': 'draw',
-                'scissors': 'Player 2'
-            },
-            'scissors': {
-                'stone': 'Player 2',
-                'paper': 'Player 1',
-                'scissors': 'draw'
-            }
-        };
-
+    String play(String choice1, String choice2) {
         return rulesMap[choice1][choice2];
     }
 
