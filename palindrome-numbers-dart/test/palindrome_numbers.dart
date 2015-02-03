b3da36f92296b36f2f0c6135e6f02a4c003da599
rchavarria <txingo@gmail.com>
2015-02-03 16:16:45 +0100

    Create hook up test

---
+++ palindrome-numbers-dart/test/palindrome_numbers.dart
@@ -0,0 +1,14 @@
+import 'package:unittest/unittest.dart';
+
+void main() {
+
+    group('Hooks', () {
+
+        test('hooks up test', () {
+            expect(2, equals(3 - 1));
+        });
+
+    });
+
+}
+
